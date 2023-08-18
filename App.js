29950a639fd91fb1087241cbb83116d6ad213892
Girish <girishvinr@gmail.com>
2023-08-18 20:25:06 +0530

    SwiperSlide integrated for card flow effects

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
 
 import { StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native';
 import React, {useRef} from 'react';
+
 
 const SCREEN_HEIGHT = Dimensions.get('window').height
 const SCREEN_WIDTH = Dimensions.get('window').width
@@ -24,24 +34,46 @@ export default function App() {
   return (
     // 
     <View style={styles.container}>
-      <Animated.View
-          style={[
-            { transform: [{translateX: pan.x}, {translateY: pan.y}]},
-          ]}
-          {...panResponder.panHandlers}
-        >
-        <View style={styles.cardView} />
-        </Animated.View>
+      
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+        loop={true}
+        momentum="true"
+        speed={400}
+        style={styles.cardContainer}
+        spaceBetween={20}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+
+        {Flyer.map((item) => (
+
+          <SwiperSlide key={item.id} style={styles.cardView} >
+           
+            <View style={{backgroundColor: item.color, display: "flex",height: "100%", borderRadius: 15}}> 
+
+              <View>
+                <Text style={styles.messageTitleText}>{item.title}</Text>
+              </View>
+              <View>
+                <Text style={styles.messageBodyText}>{item.bulletin}</Text>
+              </View>
+
+            </View>
+
+
+          </SwiperSlide>
+        ))}    
+      
+
+      </Swiper>
       <StatusBar style="auto" />
     </View>
   );
-}
-
-function renderCards(pan, panResponder){
-
-  return Foods.map((item, i) => {
-    
-  });
 }
 
 
@@ -54,26 +86,69 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  //* Swiper
+  cardContainer:{
+    flex: 1,
+    height: SCREEN_HEIGHT,
+    width: SCREEN_WIDTH -20 ,    
+    backgroundColor: 'silver',
+    padding: 10,
+
+  },
+
+  //* SwiperSlide
   cardView:{
-    backgroundColor: '#444',
-    height: SCREEN_HEIGHT - 120,
-    width: SCREEN_WIDTH - 40,
-    // padding: 10,
+    backgroundColor: '#fff',
+    height: SCREEN_HEIGHT - 60,
+    width: SCREEN_WIDTH - 80, // *Redundant - since we are using slidePerView = 1 *//
+    // padding: 50,
     // position:'absolute',
-    // resizeMode: "cover",
-    borderRadius: 10
+    resizeMode: "cover",
+    borderRadius: 15,
     // alignItems: 'center',
-    // justifyContent: 'center',    
-  }
+    // justifyContent: 'center',  
+    // minimumVelocity: 0.1
+
+  },
+
+  //* flex view on which all the eliments are placed and background color is applied
+  innerView:{
+    // backgroundColor: '#f0f',
+    // alignItems: 'center',
+    // justifyContent: 'center', 
+    display: "flex",
+    height: "100%",
+    borderRadius: 15,
+
+
+  },
+
+  messageTitleText:{
+    fontWeight:'bold',
+    textAlign:'center',
+    fontSize:20,
+    marginBottom:20,
+    marginTop:20,
+  },
+
+  messageBodyText:{
+    textAlign:'center',
+    fontSize:18,
+    marginBottom:20,
+    marginTop:20,
+  },
+
+  
 });
 
 
-const Foods = [
-  { id: "1", uri: require('./assets/img0.png') },
-  { id: "2", uri: require('./assets/img1.jpeg') },
-  { id: "3", uri: require('./assets/img2.png') },
-  { id: "4", uri: require('./assets/img3.jpeg') },
-  { id: "5", uri: require('./assets/img4.jpeg') },
+const Flyer = [
+  { id: "1", uri: require('./assets/img0.png'), color:"#FD3A73", title:"Hi", bulletin:"How are you rdoing?" },
+  { id: "2", uri: require('./assets/img1.jpeg'), color:"#424242", title:"Hello", bulletin:"How is it going on?" },
+  { id: "3", uri: require('./assets/img2.png'), color:"#FD3A73", title:"Hey", bulletin:"You look awesome!" },
+  { id: "4", uri: require('./assets/img3.jpeg'), color:"green", title:"Hola", bulletin:"What is up?" },
+  { id: "5", uri: require('./assets/img4.jpeg'), color:"#424242", title:"Yello", bulletin:"What good in the woods?" },
 ]
 
 
